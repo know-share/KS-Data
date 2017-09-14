512435381717f0502c2c4addff639020413d87a5
pabloaGaitan <pabloagaitanb@hotmail.com>
2017-09-13 20:33:35 -0500

    creacion de ideaFact

---
+++ src/main/java/com/knowshare/fact/rules/IdeaFact.java
@@ -0,0 +1,67 @@
+/**
+ * 
+ */
+package com.knowshare.fact.rules;
+
+/**
+ * @author Pablo
+ *
+ */
+public class IdeaFact {
+	
+	private String idIdea;
+	private Double distancia;
+	private Boolean arriba;
+	
+	public IdeaFact(String id , Double dist,Boolean b){
+		idIdea = id;
+		distancia = dist;
+		arriba = b;
+	}
+	
+	/**
+	 * @return the arriba
+	 */
+	public Boolean getArriba() {
+		return arriba;
+	}
+
+
+	/**
+	 * @param arriba the arriba to set
+	 */
+	public void setArriba(Boolean arriba) {
+		this.arriba = arriba;
+	}
+
+
+	/**
+	 * @return the idIdea
+	 */
+	public String getIdIdea() {
+		return idIdea;
+	}
+	
+	/**
+	 * @param idIdea the idIdea to set
+	 */
+	public void setIdIdea(String idIdea) {
+		this.idIdea = idIdea;
+	}
+	
+	/**
+	 * @return the distancia
+	 */
+	public Double getDistancia() {
+		return distancia;
+	}
+	
+	/**
+	 * @param distancia the distancia to set
+	 */
+	public void setDistancia(Double distancia) {
+		this.distancia = distancia;
+	}
+	
+	
+}
---
+++ src/main/java/com/knowshare/fact/rules/TipoIdeaRecomendacionEnum.java
@@ -0,0 +1,14 @@
+/**
+ * 
+ */
+package com.knowshare.fact.rules;
+
+/**
+ * @author Pablo
+ *
+ */
+public enum TipoIdeaRecomendacionEnum {
+	CERCANA,
+	LEJANA,
+	MUY_LEJANA;
+}
